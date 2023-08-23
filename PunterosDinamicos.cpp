2cf34667fa9f65bff2776b46102f97cf780fcce5
DannyMonge1 <142948494+DannyMonge1@users.noreply.github.com>
2023-08-22 19:55:14 -0600

    initial structures to main

---
+++ PunterosDinamicos.cpp
@@ -0,0 +1,17 @@
+#include "punterosdinamicos.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+    int sizearray = 0;
+    cout << "Enter number of students"<<endl;
+    cin>>sizearray;
+    int** arr = new int* [sizearray];
+    std::string subjects[] = { "1T-Spanish","2T-Spanish","3T-Spanish","1T-Math","2T-Math","3T-Math",
+    "1T-English","2T-English",
+    "3T-English","1T-Science",
+    "2T-Science","3T-Science" };
+   // dynamicpoint::getSignaturesData(arr, sizearray);
+
+}
+
---
+++ punterosdinamicos.h
@@ -0,0 +1,16 @@
+#pragma once
+class dynamicpoint {
+public:
+
+
+	/*void getSignaturesData(int arr, int sizearray) {
+		for (int i = 0; i < 12; i++) {
+			for (int j = 0; j < sizearray; j++)
+			{
+
+			}
+			;
+		}
+}
+	*/
+};
